658afc83143ec560846bc02402aefcdcc63dba52
Johnny Fuchs <johnnyfuchs@gmail.com>
2013-04-07 22:19:27 -0500

    Adding comments to steam game scrapping script.

--- SteamGameScrape.rb
+++ SteamGameScrape.rb
@@ -12,6 +12,9 @@ class SteamGameStrape
         @ele
     end
 
+    # Incremnts the page count.
+    # Loads the network request results into a Nokogiri HTML document.
+    # Returns the array of formated and filtered results.
     def next_page
         @page = @page.next
         doc_content = open("#{@url}?page=#{@page}", "User-Agent" => @user_agent)
@@ -20,6 +23,9 @@ class SteamGameStrape
         page_results
     end
 
+    # Finds all the blocks that match .search_result_row using a css selector.
+    # Sets the @ele reference to the next result.
+    # All parsing will be done on this object.
     def parse_page
         rows = @doc.css('.search_result_row')
         results = []
@@ -34,10 +40,13 @@ class SteamGameStrape
         results
     end
 
+    # Just joins all the arguments with tabs.
+    # This is where some db updating could happen.
     def format_next_row( id, price, name, type)
         "#{@row.to_s}\t#{id}\t#{type}\t#{price}\t#{name}"
     end
 
+    # Parse out the content type, ie: game, downloadable content, video preview, etc.
     def game_type
         img_ele = @ele.css('.search_type img')
         if img_ele && img_ele.length && img_ele.first.attribute('src')
@@ -48,18 +57,21 @@ class SteamGameStrape
         type
     end
 
+    # Parse out the Steam game reference id from the link to the detail page.
     def game_id
         app = @ele.attribute('href').content.scan(/app\/[0-9]+/)
         id = app.first ? app.first.gsub(/app\//,'') : nil
         id
     end
     
+    # Get the game name.
     def game_name
         raw_name = @ele.css('.search_name h4').first
         # Killing special characters, Encoding::ISO_8859_1 was a pain to convert to UTF-8 versions.
         raw_name ? raw_name.content.gsub(/[\s]+/, " ").gsub(/[^a-zA-Z0-9 :\-\&\.,]/, '') : ''
     end
     
+    # Parse out the game price... for some reason.
     def game_price
         raw_price = @ele.css('.search_price').first.content.split('$').last
         raw_price ? raw_price : 'Free to Play'
@@ -68,14 +80,17 @@ end
 
 
 #
-# Executing script
+# Executing script (main)
 #
 ss = SteamGameStrape.new
 
+# As long as results come back, keep fetching new pages.
 while res = ss.next_page do
     res.each do |row|
         puts row
     end
+    
+    # Stall execution for five seconds to be nice to Steam servers.
     sleep( 5 )
 end
 
